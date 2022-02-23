44d468ce1bba94a53b842479d3dfd0d47ddd33ac
natmayto <natmaya98@gmail.com>
2022-02-23 18:14:22 -0500

    ProyectoFinal

--- src/main/java/kronos.java
+++ src/main/java/kronos.java
@@ -1,25 +1,13 @@
 
 public class kronos {
 
-	 public static void main(String[] args) {
+	 	public static void main(String[] args) {
 
-	        int segundos= 100;
-
-	        for (int i = 0; i < segundos; i++) {
-	            System.out.println(i+1);
-	            delaySegundos();
-	        }
+		 System.out.println("Hola Mundo");
 
 
 	    }
 
-
-	    public static void delaySegundos() {
-	        try{
-	            Thread.sleep(1000);
-	        }catch(InterruptedException e){
-
-	        }
-	    }
-	
 }
+	
+
